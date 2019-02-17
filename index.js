b014225c4533309490e53f511103120475a423b4
Muhammad <m.n.shayyah@gmail.com>
2019-02-17 18:13:34 +0200

    add new line

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const http = require('http');
+
+
 var app=require('express')();
 var server = require('http').Server(app);
-server.listen(3000);
+const port=process.env.PORT || 3000
+server = http.createServer((req, res) => {
+res.statusCode = 200;
+res.setHeader('Content-Type', 'text/html');
+res.end('<h1>Hello Salim and ali</h1>');
+});
+server.listen(port,() => {
+console.log(`Server running at port `+port);
+});
+
 var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var bodyParser = require('body-parser');
