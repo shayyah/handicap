056c73d32aa96674556c26557831d1ff19d2844b
Muhammad <m.n.shayyah@gmail.com>
2019-02-17 19:45:48 +0200

    testing done

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port=process.env.PORT || 3000
 server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello Salim shebli and aliii habieb and shayah samh grzah</h1>');
+res.end('<h1>Hello Salim shebli and aliii habieb and shayah</h1>');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
