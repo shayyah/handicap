efb87f34fc073bc41bd4ed80df79129e7e22f9d3
Muhammad <m.n.shayyah@gmail.com>
2019-02-17 18:29:00 +0200

    add salim second name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port=process.env.PORT || 3000
 server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello Salim and aliii habieb</h1>');
+res.end('<h1>Hello Salim shebli and aliii habieb</h1>');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
