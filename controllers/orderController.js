98d024e57884b8cd9d2296374bd1e010fa932ae4
AliHabieb <38988608+AliHabeeb@users.noreply.github.com>
2019-02-20 13:10:03 +0200

    update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -9,6 +9,28 @@ var Path = 'D://ali-h/blind support/audio/orders/';
 const fs = require('fs');
 // Handle index actions
 exports.index = function (req, res) {
+  var state = req.query.order_state;
+  var skipping = 1;
+  if (req.query.page <= 1) {
+    skipping = 0;
+  } else {
+    skipping *= ((req.query.page-1) * 10);
+  }
+  if (state) {
+    Order.find({order_state : state}, null, {skip : skipping}, (err, orders) => {
+      if (err) {
+          res.json({
+              status: "error",
+              message: err,
+          });
+      }
+      res.json({
+          status: "success",
+          message: "orders retrieved successfully",
+          data: orders
+      });
+    }).limit(10);
+  } else {
     Order.get(function (err, orders) {
         if (err) {
             res.json({
@@ -22,19 +44,28 @@ exports.index = function (req, res) {
             data: orders
         });
     });
+  }
 };
 
 // Handle create order actions
 exports.new = function (req, res) {
     var order = new Order();
-    order.date = req.body.date ? req.body.date : order.date;
-    order.order_recording = req.body.order_recording;
-    order.order_state = req.body.order_state;
-    order.last_page_downloaded = req.body.last_page_downloaded;
+    order.date = new Date();
+    var bytesStream = req.body.order_recording;
+    order.order_state = false;
+    order.last_page_downloaded = 0;
     order.book_title = req.body.book_title;
     order.blind_id = req.body.blind_id;
     order.volunteer_id = req.body.volunteer_id;
     order.book_id = req.body.book_id;
+
+    // handling bytes stream
+    customPath = (Path + order._id + '.mp3');
+    order.order_recording = customPath;
+    fs.writeFile(customPath, bytesStream, (err) => {
+      if(err) res.send(err);
+      console.log('Order recoriong have been saved successfully!!');
+    });
 
     // save the order and check for errorss
     order.save(function (err) {
@@ -64,11 +95,12 @@ exports.update = function (req, res) {
 Order.findById(req.params.order_id, function (err, order) {
         if (err)
             res.send(err);
-        order.date = req.body.date ? req.body.date : order.date;
-        order.order_recording = req.body.order_recording ? req.body.order_recording : order.order_recording;
         order.order_state = req.body.order_state ? req.body.order_state : order.order_state;
         order.last_page_downloaded = req.body.last_page_downloaded ? req.body.last_page_downloaded : order.last_page_downloaded;
         order.book_title = req.body.book_title ? req.body.book_title : order.book_title;
+        if (order.book_title) {
+          order.order_state = true;
+        }
         order.blind_id = req.body.blind_id ? req.body.blind_id : order.blind_id;
         order.volunteer_id = req.body.volunteer_id ? req.body.volunteer_id : order.volunteer_id;
         order.book_id = req.body.book_id ? req.body.book_id : order.book_id;
