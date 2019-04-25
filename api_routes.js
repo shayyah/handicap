8cd89812a7062b2cb8f8b1f6dca5abf6b5aed54d
salim <salim@joybox.com>
2019-04-25 13:20:39 +0300

    Update api_routes.js

--- api_routes.js
+++ api_routes.js
@@ -3,6 +3,10 @@
 let router = require('express').Router();
 // Set default API response
 router.get('/', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+   res.setHeader('Access-Control-Allow-Credentials', true);
     res.json({
         status: 'API is Working',
         message: 'Welcome to the library sub-system of the blind support application',
