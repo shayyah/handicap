eec014bc2680dfc069eb90f126fcc122ccca5964
salim <salim@joybox.com>
2019-04-25 13:16:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,17 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'https://almacfufin.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
+ // Request headers you wish to allow
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+ // Set to true if you need the website to include cookies in the requests sent
+ // to the API (e.g. in case you use sessions)
+ res.setHeader('Access-Control-Allow-Credentials', true);
+
+ // Pass to next layer of middleware
+ next();
 });
 // Launch app to listen to specified port
 //app.listen(port, function () {
