4a1c3a1c77f3b05d319b0dcac5c96f723fdab087
salim <salim@joybox.com>
2019-04-28 16:48:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/Content/')));
+app.use(express.static(path.join(__dirname, './Content')));
 // Connect to Mongoose and set connection variable
 var db_uri = process.env.MONGODB_URI || process.env.MONGOHQ_URL ||'mongodb://127.0.0.1:27017/blind_support_data';
 //  console.log(db_uri);
