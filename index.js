41496e24d491a7a06579c1e552ab6d0a9b9ffc9e
salim <salim@joybox.com>
2019-07-08 10:56:47 +0300

    add video

--- index.js
+++ index.js
@@ -5,25 +5,35 @@ let bodyParser = require('body-parser');
 // Import Mongoose
 let mongoose = require('mongoose');
 // Initialize the app
-let app = express();
+//let app = express();
 // Import routes
 let apiRoutes = require("./api_routes");
+var Hapi = require('hapi');
+var Route = require('./route');
+var Config = require('./config');
 var path = require('path');
-var server = require('http').Server(app);
+//var server = require('http').Server(app);
+var server = new Hapi.Server();
 var serveStatic = require('serve-static');  // serve static files
-var io = require('socket.io')(server);
+//
 var cors = require('cors');
 var fs=require('fs');
 admin = require("firebase-admin");
 
-
-
+var app = {};
+app.config = Config;
+var chatapp=server.select('app');
+var io = require('socket.io')(chatapp.listener);
 // Configure bodyparser to handle post requests
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-app.use(serveStatic('locations', {'locations': ['locations.html']}));
+//app.use(bodyParser.urlencoded({
+//    extended: true
+//}));
+//app.use(bodyParser.json());
+
+
+//app.use(serveStatic('locations', {'locations': ['locations.html']}));
+
+
 //app.use(express.static(path.join(__dirname, './Content')));
 // Connect to Mongoose and set connection variable
 var db_uri = process.env.MONGODB_URI || process.env.MONGOHQ_URL ||'mongodb://127.0.0.1:27017/blind_support_data';
@@ -38,7 +48,7 @@ var db = mongoose.connection;
 // Setup server port
 var port = process.env.PORT || 3000;
 console.log(port);
-app.use(cors());
+
 
 var serviceAccount = require("./serviceAccountKey.json");
 
@@ -47,10 +57,7 @@ admin.initializeApp({
   databaseURL: "https://handicap-cfcb0.firebaseio.com"
 });
 // Send message for default URL
-app.get('/', function(req, res) {
-  res.send('Server Working on port '+port);
-
-});
+/*
 app.get('/locations', function(req, res) {
   fs.readFile('locations/index.htm',function (err, data){
        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
@@ -58,31 +65,32 @@ app.get('/locations', function(req, res) {
        res.end();
    });
 });
+
+*/
 // Use Api routes in the App
-app.use('/api', apiRoutes);
-/*app.use(function (req, res, next) {
+//app.use('/api', apiRoutes);
+server.connection({ routes: { cors: true }, port: app.config.server.port });
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
- // Request headers you wish to allow
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
- // Set to true if you need the website to include cookies in the requests sent
- // to the API (e.g. in case you use sessions)
- res.setHeader('Access-Control-Allow-Credentials', true);
-
- // Pass to next layer of middleware
- next();
-});*/
-// Launch app to listen to specified port
-//app.listen(port, function () {
-//   console.log("Running blind_support server on port " + port);
-//});
-server.listen(port,function(){
-  console.log("Running blind_support server on port " + port);
+server.register(require('inert'));
+server.register(require('vision'), function (error) {
+  if (error) {
+    console.log('Failed to load vision.');
+  }
 });
+
+server.route(Route.endpoints);
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  relativeTo: __dirname,
+  path: './view'
+});
+server.start(function() {
+  console.log('Server started at ' + server.info.uri + '.');
+});
+
 var UserController = require('./controllers/userController');
 var ConversationController =require('./controllers/conversationController');
 
